ff0477040a28d9249db53c86d22decc06f939151
Vivek Dagar <vivekdagar2017@gmail.com>
2021-09-15 18:25:23 +0530

    Program 2

---
+++ Fahrenheit-Celsius table V2.c
@@ -0,0 +1,16 @@
+/*Program 2: Fahrenheit-Celsius table V2*/
+#include <stdio.h>
+main()
+{
+    float fahr, celsius;
+    int lower, upper, step;
+    lower = 0; /*Lower limit of temp. table*/
+    upper = 300; /*upper limit*/
+    step = 20; /*step size*/
+    fahr = lower;
+    while (fahr <= upper){
+        celsius = (5.0/9.0) * (fahr-32.0);
+        printf("%3.0f %6.1f\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+}
