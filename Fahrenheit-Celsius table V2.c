16b3d61c858a210d8524dcbce6c52203971325d1
Vivek Dagar <vivekdagar2017@gmail.com>
2021-09-15 18:54:19 +0530

    program 6 and 7

--- Fahrenheit-Celsius table V2.c
+++ Fahrenheit-Celsius table V2.c
@@ -7,6 +7,7 @@ main()
     lower = 0; /*Lower limit of temp. table*/
     upper = 300; /*upper limit*/
     step = 20; /*step size*/
+    printf("Fahr Celsius table\n")
     fahr = lower;
     while (fahr <= upper){
         celsius = (5.0/9.0) * (fahr-32.0);
